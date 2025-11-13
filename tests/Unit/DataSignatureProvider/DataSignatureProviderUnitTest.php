23c22cc5cf2b32ee5ecb0c6450a1d7caa8519971
MarcMalha <marc.malha@beanstock.com>
2025-11-13 20:07:21 +0100

    Test: add unit tests for the DataSignatureProvider service responsible for signing payloads and verifying existing signatures

---
+++ tests/Unit/DataSignatureProvider/DataSignatureProviderUnitTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Unit\DataSignatureProvider;
+
+use App\Services\DataSignatureProvider\Contracts\DataSignatureProvider;
+use Tests\TestCase;
+
+class DataSignatureProviderUnitTest extends TestCase
+{
+    public function test_signature_depends_on_input_payload_value(): void
+    {
+        $payload = [
+            'message' => 'Hello World',
+            'timestamp' => 1616161616,
+            'order' => [
+                'hello' => [
+                    '1' => 1,
+                    '2' => 2,
+                ],
+                'world' => 'qwerqwer',
+            ],
+        ];
+        $flippedKeysPayload = [
+            'timestamp' => 1616161616,
+            'order' => [
+                'world' => 'qwerqwer',
+                'hello' => [
+                    '2' => 2,
+                    '1' => 1,
+                ],
+            ],
+            'message' => 'Hello World',
+        ];
+
+        /** @var DataSignatureProvider $dataSignatureProvider */
+        $dataSignatureProvider = app()->make(DataSignatureProvider::class);
+
+        $this->assertTrue(hash_equals($dataSignatureProvider->sign($payload), $dataSignatureProvider->sign($flippedKeysPayload)));
+    }
+
+    public function test_data_signature_provider_can_verify_signature(): void
+    {
+        /** @var DataSignatureProvider $dataSignatureProvider */
+        $dataSignatureProvider = app()->make(DataSignatureProvider::class);
+
+        $data = [
+            'message' => 'Hello World',
+            'timestamp' => 1616161616,
+            'order' => [
+                'hello' => [
+                    '1' => 1,
+                    '2' => 2,
+                ],
+                'world' => 'qwerqwer',
+            ],
+        ];
+
+        $signature = $dataSignatureProvider->sign($data);
+
+        $this->assertTrue($dataSignatureProvider->verify($signature, $data));
+    }
+
+    public function test_data_signature_provider_can_reject_invalid_signature(): void
+    {
+        /** @var DataSignatureProvider $dataSignatureProvider */
+        $dataSignatureProvider = app()->make(DataSignatureProvider::class);
+
+        $data = [
+            'message' => 'Hello World',
+            'timestamp' => 1616161616,
+            'order' => [
+                'hello' => [
+                    '1' => 1,
+                    '2' => 2,
+                ],
+                'world' => 'qwerqwer',
+            ],
+        ];
+
+        $this->assertFalse($dataSignatureProvider->verify('1nv4lid_s1gn4ture', $data));
+    }
+}
