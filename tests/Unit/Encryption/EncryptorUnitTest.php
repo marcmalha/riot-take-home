9a1468355e4a1351384b7fcb8ab4643daa782bb3
MarcMalha <marc.malha@beanstock.com>
2025-11-13 17:32:45 +0100

    Refactor(Tests): separate controller tests from Encryptor unit tests

---
+++ tests/Unit/Encryption/EncryptorUnitTest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Tests\Unit\Encryption;
+
+use App\Services\Encryptor\Contracts\Encryptor;
+use Tests\TestCase;
+
+use function PHPUnit\Framework\assertEqualsCanonicalizing;
+use function PHPUnit\Framework\assertFalse;
+
+class EncryptorUnitTest extends TestCase
+{
+    protected Encryptor $encryptor;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->encryptor = app()->make(Encryptor::class);
+    }
+
+    public function test_encryptor_returns_same_keys_as_payload(): void
+    {
+        $dataToEncrypt = [
+            'name' => 'John Doe',
+            'age' => 30,
+            'phone' => '123-456-7890',
+        ];
+
+        $encryptedData = $this->encryptor->encrypt($dataToEncrypt);
+
+        assertEqualsCanonicalizing(array_keys($dataToEncrypt), array_keys($encryptedData));
+    }
+
+    public function test_encryptor_encrypts_only_at_depth_1(): void
+    {
+        $dataToEncrypt = [
+            'name' => 'John Doe',
+            'age' => 30,
+            'contact' => [
+                'email' => 'john@example.com',
+                'phone' => '123-456-7890',
+            ],
+        ];
+
+        $encryptedData = $this->encryptor->encrypt($dataToEncrypt);
+
+        $isMultiDepth = collect($encryptedData)
+            ->dot()
+            ->hasAny([
+                'contact.email',
+                'contact.phone',
+            ]);
+
+        assertFalse($isMultiDepth);
+    }
+
+    public function test_decryptor_keeps_types_unchanged(): void
+    {
+        $dataToEncrypt = [
+            'age' => 30,
+            'contact' => [
+                'email' => 'john@example.com',
+                'phone' => '123-456-7890',
+            ],
+        ];
+
+        $encryptedData = $this->encryptor->encrypt($dataToEncrypt);
+
+        $decryptedData = $this->encryptor->decrypt($encryptedData);
+
+        assertEqualsCanonicalizing($dataToEncrypt, $decryptedData);
+    }
+
+    public function test_decryption_keeps_unencyrpted_data_unchanged(): void
+    {
+        $dataToEncrypt = [
+            'name' => 'John Doe',
+            'age' => 30,
+            'contact' => [
+                'email' => 'john@example.com',
+                'phone' => '123-456-7890',
+            ],
+        ];
+
+        $encryptedData = $this->encryptor->encrypt($dataToEncrypt);
+
+        $additionalProperty = [
+            'birth_date' => '1998-11-19',
+        ];
+        $encryptedDataWithAdditionalProperty = collect($encryptedData)
+            ->merge($additionalProperty)
+            ->toArray();
+        $dataToEncryptWithAdditionalProperty = collect($dataToEncrypt)
+            ->merge($additionalProperty)
+            ->toArray();
+
+        $decryptedData = $this->encryptor->decrypt($encryptedDataWithAdditionalProperty);
+
+        assertEqualsCanonicalizing($dataToEncryptWithAdditionalProperty, $decryptedData);
+    }
+}
